a4e53053d5734cac971ffe20610c2890bdcbbe1b
Vaibhav Chaurasia <vchaurasia@dminc.com>
2023-05-31 14:04:38 +0530

    First Commit

---
+++ app/src/main/java/com/baa/heathrow/stage/ArtOfGoodTestCasesActivity.kt
@@ -0,0 +1,11 @@
+package com.baa.heathrow.stage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtOfGoodTestCasesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art_of_good_test_cases)
+    }
+}
---
+++ app/src/main/java/com/baa/heathrow/stage/SoftTestIntroActivity.kt
@@ -0,0 +1,11 @@
+package com.baa.heathrow.stage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoftTestIntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soft_test_intro)
+    }
+}
